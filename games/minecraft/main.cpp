a57f60c80c81944771ee319700f6081558c5a714
Aruncutean <52048476+Aruncutean@users.noreply.github.com>
2025-01-19 14:15:54 +0200

    add

---
+++ games/minecraft/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "../../lib/SDL/include/SDL.h"
+#include "include/Minecraft.h"
+#include "../../include/window/window.h"
+
+
+int main(int argc, char* argv[]) {
+
+
+    Game *game=new Minecraft();
+    Window *window=new Window(game);
+
+    window->init();
+
+    window->start();
+    window->stop();
+
+    return 0;
+}
---
+++ games/myGame/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "../../lib/SDL/include/SDL.h"
+#include "../minecraft/include/Minecraft.h"
+#include "myGame.h"
+#include "../../include/window/window.h"
+
+
+int main(int argc, char* argv[]) {
+
+   // Game *game=new MyGame();
+    Game *game=new Minecraft();
+    Window *window=new Window(game);
+
+    window->init();
+
+    window->start();
+    window->stop();
+
+    return 0;
+}
